c607a381d3f8b2d372019c3eb2483ae0df2397a6
Nguyen Dinh Chuong <126973877+KiritoMainBro88@users.noreply.github.com>
2025-10-24 14:15:32 +0700

    Refactor Fibonacci calculation in assembly

--- 2.asm
+++ 2.asm
@@ -2,20 +2,28 @@
 include \masm32\include\masm32rt.inc
 
 .data
-promptN db "Enter the Fibonacci number to calculate (N): ",0
-pRes    db "Fibonacci: ",0
+msgN    db "Enter the Fibonacci number to calculate (N): ",0
+msgRes  db "Fibonacci: ",0
 nl      db 13,10,0
+inbuf   db 64 dup (0)
+outbuf  db 32 dup (0)
 
 .data?
-n dd ?
+n    dd ?
+t1   dd ?
+t2   dd ?
+tmp  dd ?
+cnt  dd ?
 
 .code
 start:
-  ; ask n on the same line
-  mov  eax, sval(input("Enter the Fibonacci number to calculate (N): "))
-  mov  n, eax
+  ; Read N as text -> convert to dword
+  invoke StdOut, addr msgN
+  invoke StdIn,  addr inbuf, 63
+  invoke atodw,  addr inbuf     ; EAX = N
+  mov n, eax
 
-  ; if n <= 2 -> eax = 1
+  ; if (n <= 2) eax = 1
   mov  eax, n
   cmp  eax, 2
   jg   fib_calc
@@ -23,27 +31,32 @@ start:
   jmp  fib_out
 
 fib_calc:
-  ; t1 in EAX = 1, t2 in EDX = 1, loop ECX = n-2
-  mov  eax, 1            ; t1
-  mov  edx, 1            ; t2
-  mov  ecx, n
-  sub  ecx, 2
+  mov  t1, 1
+  mov  t2, 1
+  mov  eax, n
+  sub  eax, 2
+  mov  cnt, eax
 
 fib_loop:
-  jecxz fib_done
-  mov  ebx, eax          ; tmp = t1
-  add  ebx, edx          ; tmp = t1 + t2
-  mov  eax, edx          ; t1 = t2
-  mov  edx, ebx          ; t2 = tmp
-  dec  ecx
+  cmp  cnt, 0
+  jle  fib_done
+  mov  eax, t1
+  add  eax, t2
+  mov  tmp, eax
+  mov  eax, t2
+  mov  t1, eax
+  mov  eax, tmp
+  mov  t2, eax
+  dec  cnt
   jmp  fib_loop
 
 fib_done:
-  mov  eax, edx          ; result = t2
+  mov  eax, t2                 ; EAX holds result
 
 fib_out:
-  print OFFSET pRes
-  print str$(eax)
-  print OFFSET nl
+  invoke dwtoa, eax, addr outbuf
+  invoke StdOut, addr msgRes
+  invoke StdOut, addr outbuf
+  invoke StdOut, addr nl
   exit
 end start
